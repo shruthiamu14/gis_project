7cf0993ed3b7ba2a27e04c3bd7ac776271b0fdff
saurabhkumar1432 <saurabhkumar1432001@gmail.com>
2024-06-12 10:20:34 +0530

    faeture

--- frontend/src/components/AddFeature.js
+++ frontend/src/components/AddFeature.js
@@ -2,23 +2,27 @@
 
 import React, { useState } from 'react';
 import axios from 'axios';
-
 function AddFeatureForm() {
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handleSubmit = async(e) => {
     e.preventDefault();
     try {
         const res = await axios.post('https://gis-project.onrender.com/addfeature', {name, category, latitude, longitude} )
         console.log(res.data);
+        setSuccessMessage('Your data is successfully saved'); // Set success message
         // Reset form fields
         setName('');
         setCategory('');
         setLatitude('');
         setLongitude('');
+        setTimeout(() => {
+          setSuccessMessage(''); // Clear success message after 3 seconds
+        }, 3000);
     } catch (error) {
         console.error(error);
     }
@@ -27,6 +31,7 @@ function AddFeatureForm() {
   return (
     <div className='add-feature-form'>
       <h3>Add Feature</h3>
+      {successMessage && <div className="success-message">{successMessage}</div>}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="featureName">Feature Name:</label>
@@ -36,9 +41,9 @@ function AddFeatureForm() {
           <label htmlFor="category">Category:</label>
           <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
             <option value="">Select a category</option>
-            <option value="category1">Road</option>
-            <option value="category2">water body</option>
-            <option value="category2">hospital</option>
+            <option value="Road">Road</option>
+            <option value="Waterbody">water body</option>
+            <option value="Hospital">hospital</option>
           </select>
         </div>
         <div className="form-group">
@@ -54,5 +59,4 @@ function AddFeatureForm() {
     </div>
   );
 }
-
 export default AddFeatureForm;
